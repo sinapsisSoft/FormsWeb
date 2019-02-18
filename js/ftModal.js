0a19edcb82a8ecd8da302699ab9f25a2fbab43ff
Sinapsis Soft <sinapsis.soft.developer@gmail.com>
2019-02-18 15:34:39 -0500

    new changes, dynamic tables
    property dynamics tables

--- js/ftModal.js
+++ js/ftModal.js
@@ -3,111 +3,127 @@ Author: Diego Casallas
 Date: 06/02/2019
 Description: Validate and add the elements of the modals.
 */
-var idPanel="";
-var numCellRow=0;
-/* VIEWMODALITEM FUNCTION
-Description: Receive the modal that will be visible and the other modal will be invisible.
-*/
-function viewModalItem(index) {
-	idPanel=index;
-	let panel="panelItem_"+index;
+var idTable = 0;
+var Objpanel;
+
+function viewModalItem(select) {
+	idTable = select;
+	Objpanel = "panelItem_" + idTable;
 	var itemClass = document.getElementsByClassName('panelItems');
 	for (var i = 0; i < itemClass.length; i++) {
 		itemClass[i].classList.remove("visible");
 		itemClass[i].classList.add("invisible");
 	}
-	document.getElementById(panel).classList.remove("invisible");
-	document.getElementById(panel).classList.add("visible");
+	document.getElementById(Objpanel).classList.remove("invisible");
+	document.getElementById(Objpanel).classList.add("visible");
 };
-// FIN VIEWMODALITEM
 function getDataItems() {
-	var valueList = new Array();
-	var table;
-	var idForm = "#panelItem_" + idPanel;
-	var elemens = document.querySelector(idForm);
-	var forms = elemens.querySelectorAll('form');
-	var validate = true;
-	var count = 0;
-	var listTable = new Array('tableProperty', 'tableVehicle', 'tableBeneficiaries');
-	var table = document.getElementById(listTable[idPanel]);
-	listDataTableValue = [];
-	//getSection(section); //Pendiente revisar desde aca
-	for (var i = 0; i < forms.length; i++) {
-		for (var j = 0; j < forms[i].length; j++) {
-			var elementValue = forms[i][j].value;
-			var elementType = forms[i][j].type
-			var elementsRequire = forms[i][j].required;
-			var element = forms[i][j];
-			var nameElement = element.name;
-			if (elementType == "text" && elementsRequire) {
-				if (elementValue == null || elementValue == "" || elementValue.length == 0) {
-					validate = false;
-					element.classList.add('validateError');
-					break;
 
-				} else {
-					listDataTableValue[count] = elementValue;
-					validate = true;
-					count++;
-					element.classList.remove('validateError');
+	var elemens = document.querySelector("#" + Objpanel);
+	var objForms = elemens.querySelectorAll('form');
+	validateForm(objForms);
+};
+function validateForm(forms) {
+	let listDataTableValue = [];
+	let intCount = 0;
+	let validForm = true;
+
+	for (let i = 0; i < forms.length; i++) {
+		for (let j = 0; j < forms[i].length; j++) {
+			let elemValue = forms[i][j].value;
+			let elemType = forms[i][j].type
+			let elemRequired = forms[i][j].required;
+			let element = forms[i][j];
+			let elemName = element.name;
+			let boolValidateRadio=false;
+			if (elemRequired) {
+				if (elemType == "text") {
+					validForm = validInput(elemValue);
 				}
-			} else {
-				if (elementType == "text") {
-					listDataTableValue[count] = elementValue;
-					count++;
+				if (elemType == "number") {
+					validForm = validInput(elemValue);
 				}
-			}
-			if (elementType == "radio" && elementsRequire) {
-				if (validRadio(nameElement)) {
-					if (element.checked) {
-						validate = true;
-						listDataTableValue[count] = elementValue;
-						count++;
+				if (elemType == "radio") {
+					validForm = validRadio(elemName);
+					if(validForm){
+						boolValidateRadio=true;
 					}
-				} else {
-					validate = false;
-					break;
+				}
+				if (!validForm) {
+					element.focus();
+					alert("");
+					listDataTableValue=[];
+					return false;
+				}else{
+					if(!boolValidateRadio){
+					listDataTableValue[intCount] = elemValue;
+					intCount++;	
+
+					}
+					else{
+						if(element.checked){
+							listDataTableValue[intCount] = elemValue;
+							console.log(element.checked);
+							intCount++;	
+						}
+						
+
+					}
+					
 				}
 			}
+			
 		}
 	}
-
-	if (validate) {
-		selectCreateCondition(table, listDataTableValue);
-		//cleanItem();
-		$('#myModal').modal('hide');
-	} else {
-		alert("Verifique los campos");
-		listDataTableValue = [];
-	}
-};
-
-function selectCreateCondition(table, data) {
-	var newRow;
-
-	newRow = table.insertRow(table.rows.length);
-	newRow.id = numCellRow;
-
-	if (table.id == "tableProperty") {
-		newRow.className = "dataRowProperty";
-	} else if (table.id == "tableVehicle") {
-		newRow.className = "dataRowVehicle";
-	}
-
-	for (var j = 0; j <= data.length; j++) {
-		var newCell = newRow.insertCell(j);
-		// Append a text node to the cell
-		if (j == data.length) {
-			var newButton = document.createElement('span');
-			newButton.innerHTML = '<button class="btn btn-danger btn-sm" title="Eliminar Item"  onclick="deleteCell(' + numCellRow + ')" >x</button>';
-			newCell.appendChild(newButton);
-			numCellRow++;
-		} else {
-			newCell.appendChild(document.createTextNode(data[j]));
-		}
-	};
-};
-function deleteCell(data){
-
-	alert(data);
+	createRowTable(listDataTableValue);
 }
+function createRowTable(dataRow) {
+	let objTable = getTable();
+	
+	let numLengthTable = objTable.rows.length;
+	let newRow = objTable.insertRow(numLengthTable);
+	newRow.id = objTable.id+"Row_" + numLengthTable;
+	for (let i = 0; i <= dataRow.length; i++) {
+		let newCell = newRow.insertCell(i);
+		if (i == dataRow.length) {
+			let newButton = document.createElement('span');
+			newButton.innerHTML =
+				'<button class="btn btn-danger btn-sm" title="Eliminar Item"  onclick="deleteCell(' +
+				numLengthTable + ','+idTable+')" >x</button>';
+			newCell.appendChild(newButton);
+
+		} else {
+			newCell.appendChild(document.createTextNode(dataRow[i]));
+		}
+	}
+
+
+}
+function getTable() {
+	let arraListTable = new Array('tableProperty', 'tableVehicle', 'tableBeneficiaries');
+	return objTable = document.getElementById(arraListTable[idTable]);
+
+}
+
+function deleteCell(data,tableId) {
+	idTable=tableId;
+	let objTable = getTable();
+	let objTr = objTable.querySelectorAll('tr');
+	for (let i = 0; i < objTr.length; i++) {
+		let objTableRow=objTable.id+"Row_"+ data;
+		if (objTableRow == objTr[i].id) {
+			let row = document.getElementById(objTableRow);
+			row.parentNode.removeChild(row);
+			return false;
+		}
+	}
+
+
+}
+function validInput(element) {
+	let validate = true;
+	if (element.length == 0 || element == null || element == "") {
+		validate = false;
+	}
+	return validate;
+};
